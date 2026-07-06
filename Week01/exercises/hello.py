eccb6fbb72184efbd6050b074bf60c7aefeed02f
swapniltiwhane <swapniltiwhane@gmail.com>
2026-07-06 22:32:28 +0530

    1st program

---
+++ Week01/exercises/hello.py
@@ -0,0 +1,10 @@
+name = "Swapnil"
+experience = 15
+role = "Software Engineer"
+target = "AI Full stack developer"
+
+print("Name:", name)
+print("Today's Date:", "2026-07-06")
+print("Experience:",experience)
+print("Current Role:", role)
+print("Target Role:", target)
